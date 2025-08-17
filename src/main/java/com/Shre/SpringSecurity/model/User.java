a23dd769b35505f7217dd610fdfc5b7097f0ff00
Shreyash <shreyashshree0079@gmail.com>
2025-08-17 23:56:17 +0530

    dto

--- src/main/java/com/Shre/SpringSecurity/model/User.java
+++ src/main/java/com/Shre/SpringSecurity/model/User.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
+import org.antlr.v4.runtime.misc.NotNull;
 
 @Data
 @Entity
