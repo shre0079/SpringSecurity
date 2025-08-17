9b0d19b501bb476d4e79921fcb02750be3c0140e
Shreyash <shreyashshree0079@gmail.com>
2025-08-17 15:17:55 +0530

    first commit

---
+++ src/main/java/com/Shre/SpringSecurity/HelloController.java
@@ -0,0 +1,18 @@
+package com.Shre.SpringSecurity;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("hello")
+    public String helloController() {
+        return "Hello World";
+    }
+
+    @GetMapping("about")
+    public String about(){
+        return "Shreyash";
+    }
+}
