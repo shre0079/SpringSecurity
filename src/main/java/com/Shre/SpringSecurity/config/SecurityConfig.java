7178348b04ee667880e5991da17664863145d2fd
Shreyash <shreyashshree0079@gmail.com>
2025-08-19 19:12:36 +0530

    imports

--- src/main/java/com/Shre/SpringSecurity/config/SecurityConfig.java
+++ src/main/java/com/Shre/SpringSecurity/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.Shre.SpringSecurity.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
