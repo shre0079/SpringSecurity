65c1f23b5f5ba2b79ff7f81cb10a61d5ec8651f4
Shreyash <shreyashshree0079@gmail.com>
2025-08-19 19:36:54 +0530

    updates

--- src/main/java/com/Shre/SpringSecurity/config/SecurityConfig.java
+++ src/main/java/com/Shre/SpringSecurity/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.Shre.SpringSecurity.config;
 
+import com.Shre.SpringSecurity.filter.JwtFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,10 +14,14 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig{
+
+    @Autowired
+    private JwtFilter jwtFilter;
 
     @Autowired
     private UserDetailsService userDetailsService;
@@ -29,7 +34,8 @@ public class SecurityConfig{
                         .requestMatchers("register", "login")
                         .permitAll()
                         .anyRequest().authenticated())
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
--- src/main/java/com/Shre/SpringSecurity/service/JwtService.java
+++ src/main/java/com/Shre/SpringSecurity/service/JwtService.java
@@ -1,4 +1,4 @@
-package com.Shre.SpringSecurity;
+package com.Shre.SpringSecurity.service;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
